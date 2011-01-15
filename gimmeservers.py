88d58b0735bca1c845858a4e5bf3c6b71b214fe1
Richard Crowley <r@rcrowley.org>
2011-01-15 19:02:20 +0000

    api-key -> apikey.

--- gimmeservers.py
+++ gimmeservers.py
@@ -84,7 +84,7 @@ def api_create():
 
     Driver = get_driver(Provider.RACKSPACE)
     try:
-        conn = Driver(request.form['username'], request.form['api-key'])
+        conn = Driver(request.form['username'], request.form['apikey'])
     except InvalidCredsError:
         return _api_error(401, 'Invalid credentials.')
 
@@ -113,7 +113,7 @@ def api_destroy():
 
     Driver = get_driver(Provider.RACKSPACE)
     try:
-        conn = Driver(request.form['username'], request.form['api-key'])
+        conn = Driver(request.form['username'], request.form['apikey'])
     except InvalidCredsError:
         return _api_error(401, 'Invalid credentials.')
 
